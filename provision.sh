a45ea75ef37a12876d2bc53e7267c91bd7fdef44
Ben Vine <15706933+benjaminvine@users.noreply.github.com>
2026-09-18 15:16:33 -0400

    Full fail2ban function, pre-testing

--- provision.sh
+++ provision.sh
@@ -2,19 +2,41 @@
 
 function configure_fail2ban() {
 
-    jail_config = $(cat <<EOF[sshd]
+    # Create jail config template and path variables
+    config_changed=false
+    jail_path="/etc/fail2ban/jail.d/sshd.local"
+    jail_config=$(cat <<EOF
+[sshd]
 enabled = true
 port = ssh
 filter = sshd
 maxretry = 3
 bantime = 3600
 findtime = 600
-EOF)
+EOF
+)
 
-    # Install fail2ban if not installed
-    if((!apt list fail2ban)); then
-        apt install fail2ban
+    # Install fail2ban if not already installed
+    if ! dpkg -s fail2ban 2> /dev/null | grep -q '^Status: install ok installed'; then
+        sudo apt install -y fail2ban
     fi
 
+    # Check if jail file is present and correct, if not update
+    if [[ ! -f "$jail_path" ]] || [[ "$(<"$jail_path")" != "$jail_config" ]]; then
+        printf '%s\n' "$jail_config" | sudo tee "$jail_path" >/dev/null
+        config_changed=true
+    fi
+
+    # Check if fail2ban is enabled, if not enable
+    if ! systemctl is-enabled --quiet fail2ban; then
+        sudo systemctl enable fail2ban
+    fi
+
+    # Check if fail2ban is running, if not start
+    if ! systemctl is-active --quiet fail2ban; then
+        sudo systemctl start fail2ban
+    elif config_changed; then
+        sudo systemctl restart fail2ban
+    fi
 
 }
