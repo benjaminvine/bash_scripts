e934d865d42441eef1a854198528360c2a6dc2b6
Ben Vine <15706933+benjaminvine@users.noreply.github.com>
2026-09-18 15:09:18 -0400

    Full configure_fail2ban function, pre-testing

--- provision.sh
+++ provision.sh
@@ -1,1 +1,20 @@
 !#/bin/bash
+
+function configure_fail2ban() {
+
+    jail_config = $(cat <<EOF[sshd]
+enabled = true
+port = ssh
+filter = sshd
+maxretry = 3
+bantime = 3600
+findtime = 600
+EOF)
+
+    # Install fail2ban if not installed
+    if((!apt list fail2ban)); then
+        apt install fail2ban
+    fi
+
+
+}
